b14640795ef27e2ae57b38afa63c5072d23d4a4b
John Nickell <john@onlinemeded.org>
2021-01-17 15:26:50 -0600

    saving progress

---
+++ src/Validation/Symfony/SymfonyValidationSubscriber.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Novuso\Common\Adapter\Validation\Symfony;
+
+/**
+ * Class SymfonyValidationSubscriber
+ */
+final class SymfonyValidationSubscriber
+{
+}
